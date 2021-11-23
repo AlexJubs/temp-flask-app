c8f6f39ea9d225478805d9daf40ab89e8620a6e9
alexjubs <redpod22@gmail.com>
2021-11-23 15:30:34 +0400

    setup dirs bash script

---
+++ setup_directories.sh
@@ -0,0 +1,16 @@
+mkdir \
+  app \
+  scripts \
+  app/main \
+  app/test \
+  app/main/controller \
+  app/main/model \
+  app/main/service;
+
+touch \
+  app/__init__.py \
+  app/main/__init__.py \
+  app/test/__init__.py \
+  app/main/controller/__init__.py \
+  app/main/model/__init__.py \
+  app/main/service/__init__.py;
